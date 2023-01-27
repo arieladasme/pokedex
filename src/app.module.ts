ee6cf41c13bc89e6433f133e77e97d89732559b4
ariel <ariel.wm1@gmail.com>
2023-01-27 13:46:47 -0300

    connecting mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { join } from 'path' // paquete integrado en node
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { Module } from '@nestjs/common'
-import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose'
+import { PokemonModule } from './pokemon/pokemon.module'
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
   ],
 })
