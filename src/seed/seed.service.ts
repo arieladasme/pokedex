173dde500d283093dab893e625fe5c7c217c7e95
ariel <ariel.wm1@gmail.com>
2023-01-30 14:11:24 -0300

    insert pokemons

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
 import axios, { AxiosInstance } from 'axios'
+import { Model } from 'mongoose'
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity'
 import { PokeResponse } from './interfaces/poke-response.interface'
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>, // tambien se puede con pokemonService
+  ) {}
+
   private readonly axios: AxiosInstance = axios
 
   async executeSeed() {
-    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1')
+    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/')
       const num: number = +segments[segments.length - 2] // tomo el num que esta en la penult posi
-      console.log({ name, num })
+      await this.pokemonModel.create({ name, num })
     })
 
     return data.results
