3adc282cc168e0324273951a425be9336fce3535
ariel <ariel.wm1@gmail.com>
2023-01-30 14:39:25 -0300

    Insert multiple records(option 1)

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -15,13 +15,19 @@ export class SeedService {
   private readonly axios: AxiosInstance = axios
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany({}) // delete * from pokemon
     const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+
+    const insertPromisesArray = []
 
     data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/')
       const num: number = +segments[segments.length - 2] // tomo el num que esta en la penult posi
-      await this.pokemonModel.create({ name, num })
+      // await this.pokemonModel.create({ name, num })
+      insertPromisesArray.push(this.pokemonModel.create({ name, num }))
     })
+
+    await Promise.all(insertPromisesArray)
 
     return data.results
   }
